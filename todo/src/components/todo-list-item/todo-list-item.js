d34b61b8138ad15fa15b3ac61d25a4c69567ed19
Narek Aharonyan <Narek.Aharonyan@altacode.local>
2019-09-02 15:01:46 +0400

    Add state and interactivity

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,34 +1,47 @@
 import React, { Component } from "react";
 
 import "./todo-list-item.css";
+import { stat } from "fs";
 
 export default class TodoListItem extends Component {
   constructor() {
     super();
     this.state = {
-      done: false
+      done: false,
+      important: false
     };
     this.OnLabelClick = () => {
-      console.log(`Done ${this.props.label}`);
+      this.setState(({ done }) => {
+        return {
+          done: !done
+        };
+      });
     };
-  }
+    this.onMarkImportant = () => {
+      this.setState(({important}) => {
+        return {
+          important: !important
+        };
+      });
+    };
+  };
+
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
+
 
     let classNames = "todo-list-item";
-    if(done){
-      classNames+='done'
+    if (done) {
+      classNames += ' done'
     }
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal"
-    };
+    if (important) {
+      classNames += ' important';
+    }
     return (
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.OnLabelClick}
         >
           {label}
@@ -37,6 +50,7 @@ export default class TodoListItem extends Component {
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
